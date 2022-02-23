8441479b89648a54bc5fb085766c944ca4a952c1
Marcio Caroso <marcio.caroso@loadsmart.com>
2022-02-23 10:40:50 -0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors: {
+      black: "#0A0C1C",
+      "yankees-blue": "#1B1937",
+      "white": "#FFFFFF",
+      "medium-purple": "#AB5CDB"
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      lexend: ['Lexend Deca', 'sans-serif']
+
+    },
+    extend: {
+      spacing: {
+        "18": "4.5rem"
+      }
+    }
+  },
+  plugins: [],
+}
